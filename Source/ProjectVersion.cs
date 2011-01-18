9179c98bdfc85d960775539e6afbc43186e8c25c
Jeff Schumacher <jschumacher@russell.com>
2011-01-18 11:37:25 -0800

    Moving project to correct location. Adding Ruby to Tools folder w/ build script

---
+++ Source/ProjectVersion.cs
@@ -0,0 +1,10 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Build With Ruby Example")]
+[assembly: AssemblyCompany("42 Productions")]
+[assembly: AssemblyProduct("Build With Ruby Example")]
+[assembly: AssemblyCopyright("Copyright (c)2011 42 Productions")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
+
---
+++ rakefile.rb
@@ -0,0 +1,39 @@
+#rakefile.rb
+
+require 'rake'
+require 'albacore'
+
+task :default => [:full]
+
+
+task :full => [:clean,:assemblyInfo,:build,:specifications]
+
+task :clean do
+	FileUtils.rm_rf 'build'
+	FileUtils.rm_rf 'publish'
+end
+
+
+msbuild :build do |msb|
+	msb.properties :configuration => :AutomatedRelease
+	msb.solution = "Source/BuildWithRuby.sln"
+end
+
+
+mspec :specifications do |mspec|
+	mspec.command = "Lib/mspec/mspec.exe"
+	mspec.assemblies = "Build/BuildWithRuby.Specs.dll"
+	mspec.html_output = "Report"
+end
+
+
+desc "Assembly Version Info Generator"
+assemblyinfo :assemblyInfo do |asm|
+	asm.output_file ="Source/ProjectVersion.cs"
+	asm.title = "Build With Ruby Example"
+	asm.company_name = "42 Productions"
+	asm.product_name = "Build With Ruby Example"
+	asm.version = "0.1.0.0"
+	asm.file_version = "0.1.0.0"
+	asm.copyright = "Copyright (c)2011 42 Productions"
+end
